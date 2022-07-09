d195725c0a899d83c4390a38394df3a09df7a7c7
jojoldu <jojoldu@gmail.com>
2022-07-09 21:33:54 +0900

    기본 실행 환경 구성

--- client/src/react-query/queryClient.ts
+++ client/src/react-query/queryClient.ts
@@ -1,16 +1,19 @@
-import { createStandaloneToast } from '@chakra-ui/react';
-import { theme } from '../theme';
+// import { createStandaloneToast } from '@chakra-ui/react';
+// import { theme } from '../theme';
+//
+// const toast = createStandaloneToast({ theme });
+//
+// export function queryErrorHandler(error: unknown): void {
+//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
+//   const id = 'react-query-error';
+//   const title =
+//     error instanceof Error ? error.message : 'error connecting to server';
+//
+//   // prevent duplicate toasts
+//   toast.closeAll();
+//   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+// }
 
-const toast = createStandaloneToast({ theme });
+import { QueryClient } from 'react-query';
 
-export function queryErrorHandler(error: unknown): void {
-  // error is type unknown because in js, anything can be an error (e.g. throw(5))
-  const id = 'react-query-error';
-  const title =
-    error instanceof Error ? error.message : 'error connecting to server';
-
-  // prevent duplicate toasts
-  toast.closeAll();
-  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
-}
-
+export const queryClient = new QueryClient();
