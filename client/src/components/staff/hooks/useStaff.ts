2cac324ad9de003affb68c1da27dbc074bd4c607
jojoldu <jojoldu@gmail.com>
2022-07-09 23:04:46 +0900

    spa 앱 중앙집중 & 커스텀훅 예제 추가

--- client/src/components/staff/hooks/useStaff.ts
+++ client/src/components/staff/hooks/useStaff.ts
@@ -1,15 +1,15 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
 
-// for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -21,8 +21,8 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
-  // TODO: get data from server via useQuery
-  const staff = [];
+  const fallback = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
 
   return { staff, filter, setFilter };
 }
